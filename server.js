eeae059731975bfae0741c0c19ec91810ce8733f
gump2012 <yige2002@tom.com>
2014-06-08 15:03:04 +0800

    bug

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var http = require("http");
 
 function start(route,handle){
     function onRequest(request,response){
+       var pathname = url.parse(request.url).pathname;
        route(handle,pathname,response,request);
     }
 
