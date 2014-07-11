a14c114eefb5e058f414dbbd19ab8bd18bd6f1c0
gump2012 <yige2002@tom.com>
2014-07-11 15:39:00 +0800

    瞎写一通

--- book/photo/photoHandles.js
+++ book/photo/photoHandles.js
@@ -137,11 +137,72 @@ exports.addInfo = function(response,request){
         if(requestData != ''){
             var uid = querystring.parse(requestData).uid;
             var type = querystring.parse(requestData).type;
-            var index = querystring.parse(requestData).index;
-            var txt_type = querystring.parse(requestData).txt_type;
+            var bigimage = querystring.parse(requestData).img_big;
+            var bigimagedata = JSON.parse(bigimage);
+            var smallimage = querystring.parse(requestData).img_small;
+            var smallimagedata = JSON.parse(smallimage);
+            var txttype = querystring.parse(requestData).txt_type;
+            var buildtime = querystring.parse(requestData).build_time;
+            var infotype = querystring.parse(requestData).info_type;
+            var destime = querystring.parse(requestData).des_time;
             var txt = querystring.parse(requestData).txt;
-            var des_index = querystring.parse(requestData).des_index;
-            var des_time = querystring.parse(requestData).des_time;
+
+            var responsevalue = {"info":"-1"};
+            if(uid){
+                var infomodel = mongoose.model('info');
+
+                var item = {
+                    uid:uid
+                    ,infolist:[]
+                }
+
+                infomodel.findOne({uid:uid},function(err,doc){
+                   if(doc){
+
+                       if(infotype == "0"){
+                           saveText();
+                       }
+
+                       var infoitem = {
+                           babytype:type
+                           ,index:doc.infolist.length + 1
+                           ,img_samll:''
+                           ,img_big:''
+                           ,info_type:infotype
+                           ,build_time:buildtime
+                           ,txt:txt
+                           ,commentlist:[]
+                       };
+
+                       doc.infolist.push(infoitem);
+                       doc.save(function(err){
+                           if( err )
+                           {
+                               console.log(err);
+                           }
+                       });
+                   }else{
+                       var item = {
+                           uid:uid
+                           ,infolist:[]
+                       }
+                       var infoitem = new infomodel(item);
+                       infoitem.save(function(err,silence){
+                           if( err )
+                           {
+                               console.log(err);
+                           }
+                       });
+                   }
+                });
+            }
+            else{
+                var postData = JSON.stringify(responsevalue);
+                response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+                response.write(postData);
+                response.end();
+            }
+
         }
     });
 }
